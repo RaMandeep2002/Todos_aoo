908929b1e7eff1b505f7deb27f73a80e94155ad4
RaMandeep2002 <ramandeepsigh1511@gmail.com>
2023-10-23 00:21:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TodosInput from './components/TodosInput';
+import TodosList from './components/TodosList';
 
 function App() {
+  const [todoItems, setTodoItems] = React.useState([
+    // { todo: 'Mow the lawn', complete: false },
+    // { todo: 'Do Laundry', complete: false },
+    // { todo: 'Make Dinner', complete: false },
+  ]);
+  const createTodoItem = (todo) => {
+    const newTodoitem = [...todoItems, { todo, complete: false }];
+    setTodoItems(newTodoitem);
+  };
+
+  const deleteTodoItem = (index) => {
+    const newTodoItems = [...todoItems]
+    newTodoItems.splice(index, 1)
+    setTodoItems(newTodoItems)
+    }
+
+
+    const completeTodoItem = (index) => {
+const newTodoItems = [...todoItems];
+newTodoItems[index].complete === false
+? (newTodoItems[index].complete = true)
+: (newTodoItems[index].complete = false);
+setTodoItems(newTodoItems)
+};
+
+const updateTodoItem = (index) => {
+  const newTodoItems = [...todoItems];
+  const item = newTodoItems[index];
+  let newItem = prompt(`Update ${item.todo}?`, item.todo);
+  let todoObj = { todo: newItem, complete: false };
+  newTodoItems.splice(index, 1, todoObj);
+  if (newItem === null || newItem === "") {
+  return;
+  } else {
+  item.todo = newItem;
+  }
+  setTodoItems(newTodoItems);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todos app</h1>  
+      {/* Todos input */}
+      <TodosInput createTodoItem={createTodoItem} />
+
+      {/* todos list */}
+
+      {todoItems.map((items, index) => (
+        <TodosList key={index} index={index} item={items} deleteTodoItem=
+        {deleteTodoItem}  completeTodoItem={completeTodoItem} updateTodoItem={updateTodoItem}/>
+      ))}
     </div>
   );
 }
---
+++ src/components/TodosInput.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "../styles/todoInput.css"
+const TodosInput = ({ createTodoItem }) => {
+  const [value, setValue] = React.useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (value === '') {
+      return console.log('Please add some to-dos');
+    }
+    createTodoItem(value);
+    setValue('');
+  };
+
+  return (
+    <div id="add_todo">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          placeholder="Create ToDo"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button id="create" onClick={handleSubmit}>
+          Create
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default TodosInput;
